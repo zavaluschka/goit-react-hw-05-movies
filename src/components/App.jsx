2f19ae372ad32582a6b6ab398b269397a7a428fc
zavaluschka <38804687+zavaluschka@users.noreply.github.com>
2023-11-06 11:05:40 +0200

    pages001

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import { Routes, Route, NavLink } from "react-router-dom";
+// import styled from "styled-components";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+// import NotFound from "./pages/NotFound";
+
+// const StyledLink = styled(NavLink)`
+//   color: black;
+
+//   &.active {
+//     color: orange;
+//   }
+// `;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+    <header>
+       <nav>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+</header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
+
+      </Routes>
     </div>
   );
 };
